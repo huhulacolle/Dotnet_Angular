d0893e2007d5b2476adf4a7697071b30290eaeb2
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-13 16:20:41 +0100

    truc

--- Models/Posts.cs
+++ Models/Posts.cs
@@ -2,7 +2,6 @@
 {
     public class Posts
     {
-        public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string? Content { get; set; }
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
